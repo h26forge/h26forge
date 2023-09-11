73c8a9e6604240163b6bc0d44379d8f75cb32f2c
wrv <wrv@utexas.edu>
2023-09-10 21:55:15 -0500

    Add more flags to gen_100_videos script

--- scripts/gen_100_videos.sh
+++ scripts/gen_100_videos.sh
@@ -1,15 +1,20 @@
 cur_date=$(date +%s)
 mkdir -p "tmp/"
 output_dir="tmp/rand_100_vids_$cur_date"
-tool_args=""
-generation_args="--small --ignore-edge-intra-pred --ignore-ipcm" # --config config/default.json"
+tool_args="--mp4 --mp4-rand-size --safestart"
+generation_args="--small --ignore-edge-intra-pred --ignore-ipcm --config config/default.json"
 RUST_BACKTRACE=1
 
+echo "Saving log to $output_dir/rand_100.log"
+
 mkdir -p $output_dir
-for i in $(seq -f "%04g" 0 9999); do
+for i in $(seq -f "%04g" 0 99); do
     cmd="./h26forge $tool_args generate $generation_args -o $output_dir/video.$cur_date.$i.264"
     echo $cmd
+    echo ------------------ >> $output_dir/rand_100.log
+    echo $cmd >> $output_dir/rand_100.log
     $cmd >> $output_dir/rand_100.log 2>&1
+    echo ------------------ >> $output_dir/rand_100.log
 done
 
 echo "Log saved to $output_dir/rand_100.log"
